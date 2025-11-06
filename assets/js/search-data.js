5e3bd799de2b591445fa71ca93a9432f44e43920
seok-jin1 <59434341+seok-jin1@users.noreply.github.com>
2025-11-06 08:14:02 +0000

    Deploying to gh-pages from @ seok-jin1/seok-jin1.github.io@3532bdf3ed87808fd803a4b0c74ec93526e82908 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Download the latest CV or skim highlights from my academic and industry work.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
